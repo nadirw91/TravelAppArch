428b3f960041a63fbc65e48df542d500a9f179fb
nadir <nadir.n.williams@gmail.com>
2021-09-09 00:37:54 -0400

    Build Setup

--- search/hotel/src/main/kotlin/com/nadir/search/hotel/view/HotelSearchFragment.kt
+++ search/hotel/src/main/kotlin/com/nadir/search/hotel/view/HotelSearchFragment.kt
@@ -1,11 +1,18 @@
 package com.nadir.search.hotel.view
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.nadir.search.core.dto.TravelSearch
 import com.nadir.search.hotel.R
 import com.nadir.search.hotel.viewmodel.HotelSearchViewModel
 import kotlinx.coroutines.flow.collect
@@ -16,8 +23,20 @@ class HotelSearchFragment: Fragment() {
         lifecycleOwner = this
     )
 
-    companion object {
-        fun newInstance() = HotelSearchFragment()
+    private val location: EditText by lazy {
+        requireView().findViewById(R.id.location)
+    }
+
+    private val checkIn: TextView by lazy {
+        requireView().findViewById(R.id.check_in)
+    }
+
+    private val checkOut: TextView by lazy {
+        requireView().findViewById(R.id.check_out)
+    }
+
+    private val searchButton: Button by lazy {
+        requireView().findViewById(R.id.search)
     }
 
     override fun onCreateView(
@@ -28,11 +47,39 @@ class HotelSearchFragment: Fragment() {
         return inflater.inflate(R.layout.fragment_hotel_search, container)
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        location.doOnTextChanged { text, _, _, _ ->
+            onUserSearchChange(text.toString())
+        }
+
+        checkIn.setOnClickListener {
+            DatePickerDialog(requireContext())
+                .show()
+        }
+
+        checkOut.setOnClickListener {
+            DatePickerDialog(requireContext())
+                .show()
+        }
+
+        searchButton.setOnClickListener {
+            viewModel.navigateToSearch(
+                TravelSearch.HotelSearch(
+                    location = location.text.toString(),
+                    checkInDate = checkIn.text.toString(),
+                    checkoutDate = checkOut.text.toString()
+                )
+            )
+        }
+    }
+
     private fun onUserSearchChange(query: String) {
         lifecycleScope.launchWhenResumed {
             viewModel.lookupLocation(query)
                 .collect {
-                    // update recycler view
+                    Toast.makeText(requireContext(), "Locations For: $query", Toast.LENGTH_SHORT).show()
                 }
         }
     }
